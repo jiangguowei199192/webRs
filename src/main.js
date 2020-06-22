8c3a6ced51249c26bd2e92c28d0aec817829149d
jiangguowei <jiangguowei@163.com>
2020-06-22 19:23:12 +0800

    添加选中状态

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import axios from './axios/axios'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 // import './style/theme/index.css'
-import './style/common.css'
+import './style/common.less'
 import './assets/font/iconfont.css'
 Vue.use(ElementUI)
 Vue.prototype.$axios = axios
--- src/views/videoSystem/realVideo.vue
+++ src/views/videoSystem/realVideo.vue
@@ -16,14 +16,30 @@
             <div class="search">
               <el-input type="text" placeholder="请输入设备名称" suffix-icon="el-icon-search"></el-input>
             </div>
-            <div class="list" v-for="(item,index) in listArray" :key="index">
+            <div
+              class="list"
+              v-for="(item,index) in listArray"
+              :key="index"
+              :class="{selected:item.isSelected}"
+            >
               <p>
                 <span class="area">{{item.area}}</span>
                 <i></i>
               </p>
               <div class="btns">
-                <el-button class="visible">可见光</el-button>
-                <el-button class="infrared">红外光</el-button>
+                <el-button
+                  class="visible"
+                  :style="{backgroundColor:item.visibleIsClick?'rgba(14,90,148,1)':''}"
+                  @click.stop="changeStatus(1,index)"
+                  v-if="item.visibleText"
+                >{{item. visibleText}}</el-button>
+                <el-button
+                  class="infrared"
+                  :style="{backgroundColor:item.infraredIsclick?'rgba(14,90,148,1)':''}"
+                  @click.stop="changeStatus(2,index)"
+                  :class="{isSelected:item.infraredIsclick}"
+                  v-if="item.infraredText"
+                >{{item.infraredText}}</el-button>
               </div>
             </div>
           </template>
@@ -46,9 +62,39 @@ export default {
       showLeft: true,
       showRight: true,
       index: 0,
-      listArray: [{ area: '发展大道黄浦路1' }, { area: '发展大道黄浦路2' }, { area: '发展大道黄浦路3' }, {
-        area: '发展大道黄浦路4'
-      }]
+      listArray: [
+        {
+          area: '发展大道黄浦路1',
+          visibleText: '可见光',
+          infraredText: '红外光',
+          isSelected: false,
+          visibleIsClick: false,
+          infraredIsclick: false
+        },
+        {
+          area: '发展大道黄浦路2',
+          infraredText: '红外光',
+          isSelected: false,
+          visibleIsClick: false,
+          infraredIsclick: false
+        },
+        {
+          area: '发展大道黄浦路3',
+          visibleText: '可见光',
+          isSelected: false,
+          visibleIsClick: false,
+          infraredIsclick: false
+        },
+        {
+          area: '发展大道黄浦路4',
+          visibleText: '可见光',
+          infraredText: '红外光',
+          isSelected: false,
+          visibleIsClick: false,
+          infraredIsclick: false
+        }
+      ],
+      isClick: false
     }
   },
   methods: {
@@ -57,11 +103,20 @@ export default {
     },
     closeRightInfo (type) {
       this.showRight = type !== 1
+    },
+    changeStatus (type, index) {
+      if (type === 1) {
+        this.listArray[index].visibleIsClick = true
+        this.listArray[index].isSelected = true
+      } else {
+        this.listArray[index].infraredIsclick = true
+        this.listArray[index].isSelected = true
+      }
     }
   }
 }
 </script>
-<style lang="less">
+<style lang="less" scoped>
 .videoContainer {
   .leftContainer {
     box-sizing: border-box;
@@ -90,19 +145,19 @@ export default {
     }
     div.list {
       margin-top: 20px;
-      padding-left:10px;
-      padding-top:10px;
+      padding-left: 10px;
+      padding-top: 10px;
       height: 62px;
-      background:url(../../assets/images/list-unselected.png) no-repeat;
+      background: url(../../assets/images/list-unselected.png) no-repeat;
       p {
         margin-bottom: 8px;
         color: #1eb0fc;
-        i{
+        i {
           display: inline-block;
-          width:17px;
-          height:10px;
-          background:url(../../assets/images/live.png) no-repeat;
-          margin-left:12px;
+          width: 17px;
+          height: 10px;
+          background: url(../../assets/images/live.png) no-repeat;
+          margin-left: 12px;
         }
       }
       div.btns {
@@ -115,18 +170,21 @@ export default {
           line-height: 22px;
           border: 1px solid rgba(30, 176, 252, 1);
           padding: 0;
-          background:#10243F;
-          color:#1EB0FC;
-          text-align:right;
-          padding-right:8px;
+          background: #10243f;
+          color: #1eb0fc;
+          text-align: right;
+          padding-right: 8px;
         }
-        button.visible{
-          background:url(../../assets/images/visible.png) no-repeat  4px center;
+        button.visible {
+          background: url(../../assets/images/visible.png) no-repeat 4px center;
         }
-        button.infrared{
-            background:url(../../assets/images/infrared.png) no-repeat  4px center;
+        button.infrared {
+          background: url(../../assets/images/infrared.png) no-repeat 4px center;
         }
       }
+    }
+    div.selected {
+      background: url(../../assets/images/list-selected.png) no-repeat;
     }
   }
 }
