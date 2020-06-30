1c138cb0620892d5d1e60f9f073b2db20f1b9d8d
zhaoaoxun <zhaoaoxun@gdu-tech.com>
2020-06-30 20:24:45 +0800

    临时数据

--- src/views/decisionSystem/index.vue
+++ src/views/decisionSystem/index.vue
@@ -1,12 +1,18 @@
 <template>
   <div class="decision">
     <gMap ref="gduMap" handleType="Dashboard" :bShowBasic="true" :bShowMeasure="true"></gMap>
+    <plan class="plan"></plan>
   </div>
 </template>
 
 <script>
+import Plan from './plan.vue';
+
 export default {
   name: 'decision',
+  components: {
+    Plan
+  },
   data () {
     return {
     }
@@ -21,5 +27,16 @@ export default {
 <style lang="scss" scoped>
 .decision {
   height: 800px;
+}
+.plan {
+  position: relative;
+  width: 300px;
+  // height: 500px;
+  max-height: 500px;
+  left: 15px;
+  top: -735px;
+  border-radius: 4px;
+  background: rgb(21, 51, 77);
+  overflow-y: scroll;
 }
 </style>
