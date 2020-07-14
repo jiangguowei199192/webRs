e60756c0f1e058ac05b58e45a82c1ed7a69195da
zhaoaoxun <zhaoaoxun@gdu-tech.com>
2020-07-14 20:33:32 +0800

    设置首页

---
+++ src/views/systemSettings/components/SettingLeftItem.vue
@@ -0,0 +1,70 @@
+<template>
+  <div>
+    <div class="header">
+      <div class="headerText">{{itemData.headerTitle}}</div>
+    </div>
+    <div class="item" v-for="item in itemData.info" :key="item.id" @click="itemClicked(item.id)">
+      <img :src="item.selected ? item.selectedImgPath : item.normalImgPath" class="itemImg">
+      <span class="itemSpan" :class="item.selected ? 'itemSpanSelected' : 'itemSpanNormal' ">{{item.title}}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    itemData: {
+      type: Object,
+      required: true
+    }
+  },
+  data () {
+    return {
+    }
+  },
+  methods: {
+    itemClicked: function (id) {
+      // console.log(id);
+      this.$emit("leftBoxDidSelectedItem", id);
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .header {
+    height: 22px;
+    margin-top: 26px;
+    text-align: center;
+    background: url('../../../assets/images/Setting/setting-header.png') no-repeat center;
+    .headerText {
+      width: 57px;
+      font-size: 14px;
+      font-style: italic;
+      color: #ffffff;
+      margin-left: 131px;
+    }
+  }
+  .item {
+    height: 24px;
+    margin-top: 16px;
+    .itemImg {
+      width:24px;
+      height:24px;
+      margin: 8px 0px 0px 31px;
+    }
+    .itemSpan {
+      margin-left: 20px;
+      line-height: 40px;
+      vertical-align: top;
+      font-size: 18px;
+      color: #85cfe8;
+    }
+    .itemSpanNormal {
+      color: #85cfe8;
+    }
+    .itemSpanSelected {
+      color: #ffffff;
+    }
+  }
+</style>
---
+++ src/views/systemSettings/components/SettingRightItem.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="baseBox">
+    <div class="title">{{itemData.headerTitle}}</div>
+    <div class="table">
+      <div class="item" v-for="item in itemData.items" :key="item.id" :class="item.id ? 'otherItem' : 'firstItem' ">
+        <span class="titleSpan">{{item.title}}</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    itemData: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .baseBox {
+    width: 686px;
+    margin: auto;
+  }
+  .title {
+    color: #ffffff;
+    font-size: 18px;
+    height: 44px;
+  }
+  .table {
+    border-radius: 10px;
+    border: solid 1px #39a4dd;
+  }
+  .item {
+    height: 50px;
+    .titleSpan {
+      display: inline-block;
+      margin: 17px 0px 0px 23px;
+      color: #ffffff;
+      font-size: 14px;
+    }
+  }
+  .firstItem {
+    border-top: none;
+  }
+  .otherItem {
+    border-top: solid 1px #39a4dd;
+  }
+</style>
