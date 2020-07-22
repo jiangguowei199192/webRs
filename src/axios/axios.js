71496188bfd08d8de36e7af7479949811ac1442f
jiangguowei <jiangguowei@163.com>
2020-07-22 16:21:38 +0800

    修改api请求地址

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -5,7 +5,7 @@ import {
 } from 'element-ui'
 // 创建 axios 实例
 const service = axios.create({
-  baseURL: process.env.NODE_ENV === 'development' ? 'http://172.16.63.194:8850' : 'http://111.47.13.103:50017/gdu/', // 请求前缀
+  baseURL: process.env.NODE_ENV === 'development' ? 'http://172.16.63.148:8850' : 'http://111.47.13.103:50017/gdu/', // 请求前缀
   timeout: 10000, // 请求超时时间
   // crossDomain: true, // 设置cross跨域
   withCredentials: true // 设置cross跨域 并设置访问权限 允许跨域携带cookie信息
@@ -21,7 +21,7 @@ service.interceptors.request.use((config) => {
   }
   let token = ''
   if (config.url !== '/fms-auth-center/login') {
-    token = sessionStorage.getItem('token') || 'Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX25hbWUiOiLmma7lrpnml6DkurrmnLoiLCJhdXRob3JpdGllcyI6WyIxMDAxIl0sImp0aSI6IjdiZDA3ZTRhLTdmMzktNGRkMS05YmE4LTRlOWFmMGU3MzBkZSIsImNsaWVudF9pZCI6ImZtcyIsInNjb3BlIjpbImFsbCJdfQ.Yj6g6nlx5tucOUoXB88krHPuv7HFnZO5hp0ScFy6NXDmg6w3mOwOf3rVM_I9N2ReW6zfCkwdaVRxefYqDrJoh87xogknEuqJmR3zV4EKNMbhIIHcUz_5OT0_8jwLQezNzG08UQmqGDTRxDsxWzVIDvMXPZ-aTgjs2ySgYm0NS2V7rr6SzstcLjjQxX0FYLd9YTu5i-cGnnB6BWXMPwwJ-ClCmCDOLPnUqpml7-Jdk0k6igC4pjyoZ1N4jV5foe26Fz_kzHeKviD05phiZyGF0qDo1fi6ijRKgUyoUS3uA3o4NGL1nJLeiz9SO27-qKWLrJ9Kkj-KBkS8OmwIk9zssQ'
+    token = sessionStorage.getItem('token')
   }
   config.headers.Authorization = token
   // 判断请求方式是否为POST，进行转换格式
