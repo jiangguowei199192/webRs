166616c3c48d6c407d9415cb989fc16ae5362b6a
tongziqiang <tongziqiang@gdu-tech.com>
2020-07-23 16:32:11 +0800

    1、设备详情中设备编码显示样式调整；2、火情报警时间处理方式调整。

--- src/views/videoSystem/mixins/videoMixin.js
+++ src/views/videoSystem/mixins/videoMixin.js
@@ -1,6 +1,23 @@
 import { api } from '@/api/videoSystem/realVideo'
 import { fireApi } from '@/api/videoSystem/fireAlarm'
 import { EventBus } from '@/utils/eventBus.js'
+function timestampToFormatString (fmt, v) {
+  var date = new Date(v)
+  var o = {
+    'M+': date.getMonth() + 1, // 月份
+    'd+': date.getDate(), // 日
+    'h+': date.getHours(), // 小时
+    'm+': date.getMinutes(), // 分
+    's+': date.getSeconds(), // 秒
+    'q+': Math.floor((date.getMonth() + 3) / 3), // 季度
+    S: date.getMilliseconds() // 毫秒
+  }
+  if (/(y+)/.test(fmt)) { fmt = fmt.replace(RegExp.$1, (date.getFullYear() + '').substr(4 - RegExp.$1.length)) }
+  for (var k in o) {
+    if (new RegExp('(' + k + ')').test(fmt)) { fmt = fmt.replace(RegExp.$1, (RegExp.$1.length === 1) ? (o[k]) : (('00' + o[k]).substr(('' + o[k]).length))) }
+  }
+  return fmt
+}
 const videoMixin = {
   data () {
     return {
@@ -390,8 +407,11 @@ const videoMixin = {
                   this.fireConfirmedNum++
                 }
                 if (fire.alarmTime !== null) {
-                  fire.alarmTime = fire.alarmTime.split('.')[0].replace('T', ' ')
+                  fire.alarmTime = timestampToFormatString('yyyy-MM-dd hh:mm:ss', fire.alarmTime)
                 }
+                fire.alarmPicList.forEach(img => {
+                  img.picPath = 'http://172.16.63.158:22222' + img.picPath
+                })
                 this.totalFireWarningsArray.push(fire)
                 this.newFireWarningArray.push(fire)
               }
