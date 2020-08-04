d06f4b9f5fd71f5188f7258e06fa8ac874453907
jiangguowei <jiangguowei@163.com>
2020-08-04 14:47:37 +0800

    优化设备下线时显示问题

--- src/views/videoSystem/realVideo.vue
+++ src/views/videoSystem/realVideo.vue
@@ -397,6 +397,7 @@ export default {
     // 在线或所有设备切换
     changeOnlineOrAll (isOnline) {
       if (Number(this.isOnline) === Number(isOnline)) return
+      this.getAllDeptDevices()
       this.isOnline = isOnline
       // 如果选择在线设备
       if (this.isOnline) {
