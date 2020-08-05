e1deb0661f0c446d10edb9d50c90e2bf4331d363
zhaoaoxun <zhaoaoxun@gdu-tech.com>
2020-08-05 20:53:39 +0800

    用户管理-新增用户-职务列表

--- src/api/setting.js
+++ src/api/setting.js
@@ -1,3 +1,4 @@
 export const settingApi = {
-  queryUserPage: '/cloud-fms/sysuser/queryUserPage' // 用户管理-用户列表
+  queryUserPage: '/cloud-fms/sysuser/queryUserPage', // 用户管理-用户列表
+  getRoleList: '/cloud-fms/role/getRoleList' // 查询角色列表
 }
