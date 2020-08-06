cf514d9145e5c416ce10a4d71403ec13dc513472
yaochen <yaochen@gdu-tech.com>
2020-08-06 19:45:00 +0800

    新增时间轴刻度线拖动的功能

--- src/views/videoSystem/components/timeBar.vue
+++ src/views/videoSystem/components/timeBar.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="control">
     <div class="numContainer">
       <template v-for="(item,index) in 25">
         <span
@@ -19,7 +19,7 @@
       </template>
     </div>
     <div class="barContainer">
-      <div ref="bar" class="bar" @click="barClick">
+      <div ref="bar" class="bar" @mousedown="barClick">
         <template v-for="(item,index) in segments">
           <span
             :key="index"
@@ -27,7 +27,7 @@
             :style="'width:' + item.duration * minuteWidth + 'px;' + 'left:' + item.start * minuteWidth + 'px;'"
           ></span>
         </template>
-        <div class="pointer" ref="pointer" :style="'left:' + pointerLeft + 'px;'">
+        <div class="pointer" ref="pointer" v-drag="me">
           <span>{{curTime}}</span>
           <span class="icon"></span>
         </div>
@@ -42,8 +42,49 @@ export default {
     return {
       step: 0,
       minuteWidth: 0,
-      pointerW: 0,
-      pointerLeft: -64
+      pointerW: 0, // pointer宽度
+      pointerLeft: 0, // pointer左边距
+      me: '',
+      isDrag: false
+    }
+  },
+
+  // 注册局部组件指令
+  directives: {
+    drag: function (el, binding) {
+      const me = binding.value
+      const dragBox = el // 获取当前元素
+      dragBox.onmousedown = e => {
+        me.isDrag = true
+        // 阻止事件冒泡
+        if (e.stopPropagation) e.stopPropagation()
+        else e.cancelBubble = true
+
+        // 算出鼠标相对元素的位置
+        const disX = e.clientX - dragBox.offsetLeft
+        document.onmousemove = e => {
+          // 用鼠标的位置减去鼠标相对元素的位置，得到元素的位置
+          let left = e.clientX - disX
+          const maxLeft = me.$refs.bar.clientWidth + me.pointerLeft
+          // 左边界限制
+          if (left < me.pointerLeft) { left = me.pointerLeft }
+          // 右边限制
+          if (left > maxLeft) {
+            left = maxLeft
+          }
+          // 移动当前元素
+          dragBox.style.left = left + 'px'
+          me.toHourMinute((left - me.pointerLeft) / me.minuteWidth)
+        }
+        document.onmouseup = e => {
+          me.$emit('jumpEvent', '')
+          // 鼠标弹起来的时候不再移动
+          document.onmousemove = null
+          // 预防鼠标弹起来后还会循环（即预防鼠标放上去的时候还会移动）
+          document.onmouseup = null
+          me.isDrag = false
+        }
+      }
     }
   },
 
@@ -58,15 +99,20 @@ export default {
     }
   },
 
+  created () {
+    this.me = this
+  },
+
   mounted () {
     this.pointerW = this.$refs.pointer.clientWidth
+    this.pointerLeft = -this.pointerW + 5
+    this.$refs.pointer.style.left = this.pointerLeft + 'px'
     this.calculateStep()
     window.onresize = () => {
       this.calculateStep()
       var arry = this.curTime.split(':')
-      var minute =
-        parseInt(arry[0]) * 60 + parseInt(arry[1]) + parseInt(arry[2]) / 60
-      this.pointerLeft = minute * this.minuteWidth - this.pointerW + 5
+      var minute = parseInt(arry[0]) * 60 + parseInt(arry[1]) + parseInt(arry[2]) / 60
+      this.$refs.pointer.style.left = minute * this.minuteWidth - this.pointerW + 5 + 'px'
     }
   },
 
@@ -99,6 +145,7 @@ export default {
      */
     jumpHour (hour) {
       this.jumpMinute(hour * 60)
+      this.$emit('jumpEvent', '')
     },
 
     /**
@@ -115,7 +162,8 @@ export default {
      * @param {Integer}} minute 分钟
      */
     jumpMinute (minute) {
-      this.pointerLeft = minute * this.minuteWidth - this.pointerW + 5
+      if (this.isDrag) return
+      this.$refs.pointer.style.left = minute * this.minuteWidth - this.pointerW + 5 + 'px'
       this.toHourMinute(minute)
     },
 
@@ -124,7 +172,7 @@ export default {
      */
     barClick: function (event) {
       var x = event.offsetX
-      this.pointerLeft = x - this.pointerW + 5
+      this.$refs.pointer.style.left = x - this.pointerW + 5 + 'px'
       this.toHourMinute(x / this.minuteWidth)
       this.$emit('jumpEvent', '')
     }
@@ -132,6 +180,14 @@ export default {
 }
 </script>
 <style lang="less" scoped>
+.control {
+  //以下四行取消双击选中文字效果
+  -moz-user-select: none;
+  -ms-user-select: none;
+  -webkit-user-select: none;
+  user-select: none;
+}
+
 .numContainer {
   height: 20px;
   position: relative;
@@ -174,8 +230,8 @@ export default {
     height: 42px;
     width: 70px;
     top: -7px;
-    justify-content:space-between;
-    align-items:center;
+    justify-content: space-between;
+    align-items: center;
     :nth-child(1) {
       padding: 1px 3px;
       font-size: 13px;
@@ -185,6 +241,7 @@ export default {
       border-radius: 4px;
     }
     :nth-child(2) {
+      pointer-events: auto;
       display: inline-block;
       width: 10px;
       height: 42px;
