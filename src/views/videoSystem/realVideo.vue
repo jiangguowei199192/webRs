0b88cbde49fbee9f5a2951c9520ff04f4ea4ebff
jiangguowei <jiangguowei@163.com>
2020-08-08 09:52:02 +0800

    update

--- src/views/videoSystem/realVideo.vue
+++ src/views/videoSystem/realVideo.vue
@@ -157,7 +157,7 @@
               <div class="pagination">
                 <el-pagination
                   :page-size="showVideoPageSize"
-                  layout="prev,pager,next"
+                  layout="prev,next"
                   :total="totalVideosArray.length"
                   :current-page.sync="currentPage"
                   @prev-click="pre"
