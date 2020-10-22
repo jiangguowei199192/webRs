5f4f6a85a4c0673f261a1ad2dfc3b14d539e39ce
jiangguowei <jiangg_uowei@163.com>
2020-10-22 10:50:29 +0800

    配置文件默认读取消防外网地址

--- vue.config.js
+++ vue.config.js
@@ -12,15 +12,17 @@ const createServerConfig = function (compilation) {
     projectTitle: '天地一体化预警侦查系统',
     baseUrlList: [
       {
-        baseUrl: 'http://111.47.13.103:40009',
-        mqttServer: '111.47.13.103',
-        mqttPort: 40011,
-        headImg: 'http://111.47.13.103:40015'
-        // baseUrl: 'http://58.49.169.235:50020',
-        // mqttServer: '58.49.169.235',
-        // mqttPort: 50022,
-        // headImg: 'http://58.49.169.235:50026'
+        // baseUrl: 'http://111.47.13.103:40009',
+        // mqttServer: '111.47.13.103',
+        // mqttPort: 40011,
+        // headImg: 'http://111.47.13.103:40015'
+        // 消防外网
+        baseUrl: 'http://58.49.169.235:50020',
+        mqttServer: '58.49.169.235',
+        mqttPort: 50022,
+        headImg: 'http://58.49.169.235:50026'
       }
+      // 消防内网
       // {
       //   baseUrl: 'http://172.1.53.131:50020',
       //   mqttServer: '172.1.53.131',
