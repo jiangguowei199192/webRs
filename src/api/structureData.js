ad8e473bd30429f94d196cd1b9691eed80fdd686
yc <yaochen85@163.com>
2021-04-05 16:54:50 +0800

    告警中心详情页面跨域修复

--- src/api/structureData.js
+++ src/api/structureData.js
@@ -1,4 +1,7 @@
 export const structureApi = {
   dataList: '/stru-service/structuration/spaceTimeQuery/queryData',
-  queryAlarmList: '/stru-service/structuration/tAlarm/query'
+  queryAlarmList: '/stru-service/structuration/tAlarm/query',
+  queryAlarmRemarkList: '/stru-service/structuration/tAlarmRemark/query',
+  insertAlarmRemark: '/stru-service/structuration/tAlarmRemark/insert',
+  updateAlarm: '/stru-service/structuration/tAlarm/update'
 }
--- src/views/warningInfo/warningDetail.vue
+++ src/views/warningInfo/warningDetail.vue
@@ -96,8 +96,9 @@
   </el-dialog>
 </template>
 <script>
-import axios from 'axios'
-import qs from 'qs'
+// import axios from 'axios'
+// import qs from 'qs'
+import { structureApi } from '@/api/structureData.js'
 export default {
   props: {
     dialogVisible: {
@@ -118,24 +119,25 @@ export default {
   methods: {
     // 确认/误报
     confirmWarn (status) {
-      var xsRequest = axios.create({
-        baseURL: 'http://172.16.63.43:8080',
-        timeout: 10000,
-        withCredentials: true,
-        paramsSerializer: function (params) {
-          return qs.stringify(params, { arrayFormat: 'repeat' })
-        }
-      })
-      xsRequest.interceptors.request.use(config => {
-        config.data = qs.stringify({ ...config.data })
-        return config
-      })
+      // var xsRequest = axios.create({
+      //   baseURL: 'http://172.16.63.43:8080',
+      //   timeout: 10000,
+      //   withCredentials: true,
+      //   paramsSerializer: function (params) {
+      //     return qs.stringify(params, { arrayFormat: 'repeat' })
+      //   }
+      // })
+      // xsRequest.interceptors.request.use(config => {
+      //   config.data = qs.stringify({ ...config.data })
+      //   return config
+      // })
       const params = {
         id: this.info.id,
         status
       }
-      xsRequest
-        .post('/structuration/tAlarm/update', params)
+      // xsRequest
+      //   .post('/structuration/tAlarm/update', params)
+      this.$axios.post(structureApi.updateAlarm, params)
         .then(res => {
           if (res.data.code === 0) {
             this.$notify.success({
@@ -151,26 +153,27 @@ export default {
     },
     // 添加备注
     addRemark () {
-      const xsRequest = axios.create({
-        baseURL: 'http://172.16.63.43:8080',
-        timeout: 10000,
-        withCredentials: true,
-        paramsSerializer: function (params) {
-          return qs.stringify(params, { arrayFormat: 'repeat' })
-        }
-      })
-      xsRequest.interceptors.request.use(config => {
-        config.data = qs.stringify({ ...config.data })
-        return config
-      })
+      // const xsRequest = axios.create({
+      //   baseURL: 'http://172.16.63.43:8080',
+      //   timeout: 10000,
+      //   withCredentials: true,
+      //   paramsSerializer: function (params) {
+      //     return qs.stringify(params, { arrayFormat: 'repeat' })
+      //   }
+      // })
+      // xsRequest.interceptors.request.use(config => {
+      //   config.data = qs.stringify({ ...config.data })
+      //   return config
+      // })
       const params = {
         alarmId: this.info.id,
         authorId: JSON.parse(localStorage.getItem('userDetail')).customerId,
         authName: JSON.parse(localStorage.getItem('userDetail')).username,
         remark: this.remark
       }
-      xsRequest
-        .post('/structuration/tAlarmRemark/insert', params)
+      // xsRequest
+      //   .post('/structuration/tAlarmRemark/insert', params)
+      this.$axios.post(structureApi.insertAlarmRemark, params)
         .then(res => {
           if (res.data.code === 0) {
             this.$notify.success({
@@ -187,25 +190,26 @@ export default {
         })
     },
     getRemarkList () {
-      const xsRequest = axios.create({
-        baseURL: 'http://172.16.63.43:8080',
-        timeout: 10000,
-        withCredentials: true,
-        paramsSerializer: function (params) {
-          return qs.stringify(params, { arrayFormat: 'repeat' })
-        }
-      })
-      xsRequest.interceptors.request.use(config => {
-        config.data = qs.stringify({ ...config.data })
-        return config
-      })
+      // const xsRequest = axios.create({
+      //   baseURL: 'http://172.16.63.43:8080',
+      //   timeout: 10000,
+      //   withCredentials: true,
+      //   paramsSerializer: function (params) {
+      //     return qs.stringify(params, { arrayFormat: 'repeat' })
+      //   }
+      // })
+      // xsRequest.interceptors.request.use(config => {
+      //   config.data = qs.stringify({ ...config.data })
+      //   return config
+      // })
       const params = {
         alarmId: this.info.id,
         page: 1,
         sizeOfPage: 10000
       }
-      xsRequest
-        .post('/structuration/tAlarmRemark/query', params)
+      // xsRequest
+      //   .post('/structuration/tAlarmRemark/query', params)
+      this.$axios.post(structureApi.queryAlarmRemarkList, params)
         .then(res => {
           if (res && res.data && res.data.code === 0) {
             const data = res.data.data
